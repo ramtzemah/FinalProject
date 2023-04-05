4624521132d20deb26828d2babc7d08bf9bfbfa5
RamTzemah <ram.tzemah@inx.co>
2023-04-05 17:30:41 +0300

    Add parties list to areas

--- app/src/main/java/com/example/finalproject/Entities/Area.java
+++ app/src/main/java/com/example/finalproject/Entities/Area.java
@@ -2,13 +2,24 @@ package com.example.finalproject.Entities;
 
 import com.example.finalproject.Calculations.Generators;
 
-public class Area {
-    String id;
-    String areaName;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
-    public Area(String areaName) {
+public class Area {
+    private String id;
+    private String areaName;
+    private Map<String, Integer> partiesVotes;
+
+    public Area(String areaName, Set<String> partiesKeys) {
         this.areaName = areaName;
-        id = Generators.generateId();
+        this.id = Generators.generateId();
+        this.partiesVotes = new HashMap<>();
+        Iterator itr = partiesKeys.iterator();
+        while(itr.hasNext()){
+            partiesVotes.put((String) itr.next(), 0);
+        }
     }
 
     public String getId() {
