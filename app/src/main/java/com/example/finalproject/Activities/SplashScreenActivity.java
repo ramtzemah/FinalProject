747bafc51651c613eb73495e68b201a6324dfe56
omergean1212 <65233993+omergean1212@users.noreply.github.com>
2023-05-23 17:05:23 +0300

    all user screens are good to go only thing to implement is the arrow back in the code of the PartyDetailsActivity.java its name is *MB_home*

--- app/src/main/java/com/example/finalproject/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/finalproject/Activities/SplashScreenActivity.java
@@ -8,11 +8,15 @@ import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.finalproject.DBUtils.DbUtils;
+import com.example.finalproject.DBUtils.TemporaryDB;
 import com.example.finalproject.R;
+
+import io.realm.Realm;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SCREEN_TIMEOUT = 3000; // 3 seconds
+    private static final int SPLASH_SCREEN_TIMEOUT = 2000; // 2 seconds
 
     private ProgressBar progressBar;
 
@@ -20,21 +24,44 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_activity);
+        Realm.init(this);
+        regularFlow();
 
+        DbUtils dbUtils = new DbUtils();
+        dbUtils.initConnection();
         progressBar = findViewById(R.id.progress_bar);
         progressBar.setVisibility(View.VISIBLE);
 
-        // Simulate loading for 3 seconds
+        // Simulate loading for 2 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 progressBar.setVisibility(View.GONE);
-                moveToVoteActivity();
+                moveToWelcomeActivity();
             }
         }, SPLASH_SCREEN_TIMEOUT);
     }
+    private void regularFlow() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-    private void moveToVoteActivity() {
+        TemporaryDB.setOldestAge("oldestAge");
+        TemporaryDB.setStartVotingAge("startAge");
+        TemporaryDB.dateOfStartVotingBeforeFormat();
+        TemporaryDB.dateOfEndVotingBeforeFormat();
+        TemporaryDB.addAllVoters();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        TemporaryDB.addAllParties();
+        TemporaryDB.addAllAreas();
+    }
+    private void moveToWelcomeActivity() {
         Intent intent = new Intent(this, WelcomeActivity.class);
         startActivity(intent);
         finish();
